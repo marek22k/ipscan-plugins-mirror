2240c7aebdcd0e521ca5cc0320dce13c189f9e35
Marek Küthe <m.k@mk16.de>
2025-10-28 18:49:41 +0000

    Improve style

--- ssh-banner/src/bandura/ipscan/plugins/SSHBannerFetcher.java
+++ ssh-banner/src/bandura/ipscan/plugins/SSHBannerFetcher.java
@@ -2,6 +2,8 @@ package bandura.ipscan.plugins;
 
 import net.azib.ipscan.config.ScannerConfig;
 import net.azib.ipscan.fetchers.PortTextFetcher;
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.NonNull;
 
 public class SSHBannerFetcher extends PortTextFetcher {
 
@@ -9,6 +11,8 @@ public class SSHBannerFetcher extends PortTextFetcher {
         super(scannerConfig, 22, "", "^(.*)$");
     }
 
+    @CheckReturnValue
+    @NonNull
     public String getId() {
         return "fetcher.sshBanner";
     }
