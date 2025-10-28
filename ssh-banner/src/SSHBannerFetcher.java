baf1fae1f510a523fe2c3972f7357df3aa11cc1c
Marek Küthe <m.k@mk16.de>
2025-10-28 18:04:59 +0000

    Format

--- ssh-banner/src/SSHBannerFetcher.java
+++ ssh-banner/src/SSHBannerFetcher.java
@@ -1,16 +1,14 @@
 import net.azib.ipscan.config.ScannerConfig;
 import net.azib.ipscan.fetchers.PortTextFetcher;
-import net.azib.ipscan.config.Labels;
-import net.azib.ipscan.config.Config;
 
 public class SSHBannerFetcher extends PortTextFetcher {
 
-	public SSHBannerFetcher(ScannerConfig scannerConfig) {
-		super(scannerConfig, 22, "", "^(.*)$");
-	}
+    public SSHBannerFetcher(ScannerConfig scannerConfig) {
+        super(scannerConfig, 22, "", "^(.*)$");
+    }
 
-	public String getId() {
-		return "fetcher.sshBanner";
-	}
+    public String getId() {
+        return "fetcher.sshBanner";
+    }
 
 }
