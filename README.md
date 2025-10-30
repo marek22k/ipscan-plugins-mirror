42003b972767eb4aa39cb653b5275acbf4bd0c94
Marek Küthe <m.k@mk16.de>
2025-10-30 15:57:25 +0000

    Add daytime protocol fetcher

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ SPDX-License-Identifier: GPL-3.0-or-later
 | mikrotik-routeros-version-fetcher | Retrieves the RouterOS version via the WinBox API and returns it. |
 | echo-server-fetcher | Checks whether the echo server is working. Returns `True` if it is working and `False (n)` if it is not working, where n is the position (starting at 0) of the first byte that differs from the payload sent. 714 bytes are sent as payload. |
 | time-protocol-fetcher | Retrieves the current time from a time server (RFC848) and returns it. |
+| daytime-protocol-fetcher | Retrieves a time string via the Daytime protocol and returns it. |
---
+++ daytime-protocol-fetcher/src/bandura/ipscan/plugins/DaytimeProtocolFetcher.java
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Marek Küthe <m.k@mk16.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package bandura.ipscan.plugins;
+
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+import net.azib.ipscan.config.ScannerConfig;
+import net.azib.ipscan.fetchers.PortTextFetcher;
+
+/**
+ * Fetcher, which retrieves the daytime.
+ */
+public class DaytimeProtocolFetcher extends PortTextFetcher {
+    private static final int DAYTIME_PORT = 13;
+    private static final String REGEX = "^(.*)$";
+
+    public DaytimeProtocolFetcher(ScannerConfig scannerConfig) {
+        super(scannerConfig, DAYTIME_PORT, "", REGEX);
+    }
+
+    @CheckReturnValue
+    @NonNull
+    @Override
+    public String getId() {
+        return "fetcher.daytimeProtocolFetcher";
+    }
+
+}
