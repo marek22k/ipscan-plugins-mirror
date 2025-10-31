f7f6e2fb031b3a64f2c5bd351c3834926efd6aae
Marek Küthe <m.k@mk16.de>
2025-10-31 23:02:28 +0000

    Implement chargen fetcher

---
+++ chargen-fetcher/src/bandura/ipscan/plugins/ChargenFetcher.java
@@ -0,0 +1,106 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Marek Küthe <m.k@mk16.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package bandura.ipscan.plugins;
+
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.azib.ipscan.config.LoggerFactory;
+import net.azib.ipscan.config.ScannerConfig;
+import net.azib.ipscan.core.ScanningSubject;
+import net.azib.ipscan.fetchers.AbstractFetcher;
+
+/**
+ * Fetcher that checks whether the a Chargen is running and working.
+ */
+public class ChargenFetcher extends AbstractFetcher {
+    private static final int CHARGEN_PORT = 19;
+    private static final Logger LOG = LoggerFactory.getLogger();
+    private static final int LINES_TO_CHECK_ADDITONAL = 1;
+    private static final String RFC_PATTERN_CHARACTERS = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ";
+
+    @CheckReturnValue
+    @NonNull
+    private static String getRFCPattern(int no) {
+        int start = no % (RFC_PATTERN_CHARACTERS.length() + 1);
+        int end = Math.min(start + 72, RFC_PATTERN_CHARACTERS.length());
+        int missingCharacters = 72 - (end - start);
+        String result = RFC_PATTERN_CHARACTERS.substring(start, end);
+        if (missingCharacters != 0) {
+            result = result + RFC_PATTERN_CHARACTERS.substring(0, missingCharacters);
+        }
+        return result;
+    }
+
+    private ScannerConfig scannerConfig;
+
+    public ChargenFetcher(ScannerConfig scannerConfig) {
+        this.scannerConfig = scannerConfig;
+    }
+
+    @CheckReturnValue
+    @NonNull
+    @Override
+    public String getId() {
+        return "fetcher.chargenFetcher";
+    }
+
+    @CheckReturnValue
+    @Nullable
+    @Override
+    public Object scan(ScanningSubject subject) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(subject.getAddress(), CHARGEN_PORT), subject.getAdaptedPortTimeout());
+            socket.setTcpNoDelay(true);
+            socket.setSoTimeout(scannerConfig.portTimeout * 2);
+            socket.setSoLinger(true, 0);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), StandardCharsets.ISO_8859_1)
+            );
+            String line = in.readLine();
+            if (line == null || line.isBlank()) {
+                return "False";
+            } else {
+                subject.setResultType(ResultType.WITH_PORTS);
+                if (line.equals(getRFCPattern(0))) {
+                    for (int no = 0; no < LINES_TO_CHECK_ADDITONAL; no++) {
+                        String exprectedResponse = getRFCPattern(no + 1);
+                        String response = in.readLine();
+                        if (response == null || !response.equals(exprectedResponse)) {
+                            return "True (partial RFC patterns)";
+                        }
+                    }
+                    return "True (RFC pattern)";
+                } else {
+                    return "True (non-RFC pattern)";
+                }
+            }
+        } catch (ConnectException e) {
+            // no connection
+        } catch (SocketTimeoutException e) {
+            // no information
+        } catch (SocketException e) {
+            // connection reset
+        } catch (IOException e) {
+            LOG.log(Level.FINE, subject.getAddress().toString(), e);
+        }
+        return null;
+    }
+
+}
