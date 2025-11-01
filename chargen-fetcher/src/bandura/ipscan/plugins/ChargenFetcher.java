59f04a151f611aab96dcf8e93bb6b35237d6a093
Marek Küthe <m.k@mk16.de>
2025-11-01 00:37:43 +0000

    Add settings dialog for chargen fetcher

--- chargen-fetcher/src/bandura/ipscan/plugins/ChargenFetcher.java
+++ chargen-fetcher/src/bandura/ipscan/plugins/ChargenFetcher.java
@@ -25,6 +25,7 @@ import net.azib.ipscan.config.ScannerConfig;
 import net.azib.ipscan.core.ScanningResult.ResultType;
 import net.azib.ipscan.core.ScanningSubject;
 import net.azib.ipscan.fetchers.AbstractFetcher;
+import net.azib.ipscan.fetchers.FetcherPrefs;
 
 /**
  * Fetcher that checks whether the a Chargen is running and working.
@@ -32,12 +33,14 @@ import net.azib.ipscan.fetchers.AbstractFetcher;
 public class ChargenFetcher extends AbstractFetcher {
     private static final int CHARGEN_PORT = 19;
     private static final Logger LOG = LoggerFactory.getLogger();
-    private static final int LINES_TO_CHECK_ADDITIONALLY = 2;
     private static final String RFC_PATTERN_CHARACTERS = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ";
 
     @CheckReturnValue
     @NonNull
     private static String getRFCPattern(int no) {
+        if (no < 0) {
+            throw new IllegalArgumentException("Number is negative: " + no);
+        }
         int start = no % (RFC_PATTERN_CHARACTERS.length() + 1);
         int end = Math.min(start + 72, RFC_PATTERN_CHARACTERS.length());
         int missingCharacters = 72 - (end - start);
@@ -52,9 +55,11 @@ public class ChargenFetcher extends AbstractFetcher {
     }
 
     private ScannerConfig scannerConfig;
+    private int linesToCheckAdditionally;
 
     public ChargenFetcher(ScannerConfig scannerConfig) {
         this.scannerConfig = scannerConfig;
+        this.linesToCheckAdditionally = 2;
     }
 
     @CheckReturnValue
@@ -62,6 +67,19 @@ public class ChargenFetcher extends AbstractFetcher {
     @Override
     public String getId() {
         return "fetcher.chargenFetcher";
+    }
+
+    @CheckReturnValue
+    @NonNull
+    public int getLinesToCheckAdditionally() {
+        return this.linesToCheckAdditionally;
+    }
+
+    @CheckReturnValue
+    @NonNull
+    @Override
+    public Class<? extends FetcherPrefs> getPreferencesClass() {
+        return ChargenFetcherPrefs.class;
     }
 
     @CheckReturnValue
@@ -84,11 +102,13 @@ public class ChargenFetcher extends AbstractFetcher {
                 subject.setResultType(ResultType.WITH_PORTS);
 
                 if (line.equals(getRFCPattern(0))) {
-                    for (int no = 0; no < LINES_TO_CHECK_ADDITIONALLY; no++) {
-                        String exprectedResponse = getRFCPattern(no + 1);
+                    for (int no = 0; no < linesToCheckAdditionally; no++) {
+                        int numberOfLines = no + 1; // including 0
+                        String exprectedResponse = getRFCPattern(numberOfLines);
                         String response = in.readLine();
                         if (response == null || !response.equals(exprectedResponse)) {
-                            return "True (partial RFC pattern)";
+                            return "True (partial RFC pattern, " + (numberOfLines + 1) + "/"
+                                    + (linesToCheckAdditionally + 1) + ")";
                         }
                     }
                     return "True (RFC pattern)";
@@ -106,6 +126,13 @@ public class ChargenFetcher extends AbstractFetcher {
             LOG.log(Level.FINE, subject.getAddress().toString(), e);
         }
         return null;
+    }
+
+    public void setLinesToCheckAdditionally(int linesToCheckAdditionally) {
+        if (linesToCheckAdditionally < 0) {
+            throw new IllegalArgumentException("Value is negative: " + linesToCheckAdditionally);
+        }
+        this.linesToCheckAdditionally = linesToCheckAdditionally;
     }
 
 }
---
+++ chargen-fetcher/src/bandura/ipscan/plugins/ChargenFetcherPrefs.java
@@ -0,0 +1,89 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Marek Küthe <m.k@mk16.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package bandura.ipscan.plugins;
+
+import java.util.prefs.Preferences;
+
+import net.azib.ipscan.config.Labels;
+import net.azib.ipscan.fetchers.Fetcher;
+import net.azib.ipscan.fetchers.FetcherPrefs;
+import net.azib.ipscan.gui.AbstractModalDialog;
+import net.azib.ipscan.gui.util.LayoutHelper;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class ChargenFetcherPrefs extends AbstractModalDialog implements FetcherPrefs {
+    private ChargenFetcher fetcher;
+
+    public ChargenFetcherPrefs() {
+        super();
+        fetcher = null;
+    }
+
+    @Override
+    public void openFor(Fetcher fetcher) {
+        if (!(fetcher instanceof ChargenFetcher)) {
+            throw new IllegalArgumentException("Invalid fetcher: " + fetcher.getClass().getName());
+        }
+        this.fetcher = (ChargenFetcher) fetcher;
+        open();
+    }
+
+    @Override
+    protected void populateShell() {
+        if (fetcher == null) {
+            throw new IllegalArgumentException("Fetcher is null.");
+        }
+
+        shell = new Shell(Display.getCurrent().getActiveShell(), SWT.DIALOG_TRIM);
+        shell.setText(fetcher.getName());
+        shell.setLayout(LayoutHelper.formLayout(10, 10, 5));
+
+        Label linesLabel = new Label(shell, SWT.NONE);
+        linesLabel.setText(Labels.getLabel("text.fetcher.chargenFetcher.linesToCheckAdditionally"));
+
+        Text linesText = new Text(shell, SWT.BORDER);
+        linesText.setText(String.valueOf(fetcher.getLinesToCheckAdditionally()));
+
+        linesText.setLayoutData(
+                LayoutHelper
+                        .formData(new FormAttachment(0), new FormAttachment(100), new FormAttachment(linesLabel), null)
+        );
+        // linesLabel
+        // .setLayoutData(LayoutHelper.formData(new FormAttachment(0), null, new
+        // FormAttachment(linesText), null));
+
+        Button okButton = new Button(shell, SWT.NONE);
+        okButton.setText(Labels.getLabel("button.OK"));
+
+        Button cancelButton = new Button(shell, SWT.NONE);
+        cancelButton.setText(Labels.getLabel("button.cancel"));
+
+        positionButtonsInFormLayout(okButton, cancelButton, linesText);
+
+        okButton.addListener(SWT.Selection, e -> {
+            Preferences prefs = fetcher.getPreferences();
+
+            int lines = Integer.parseInt(linesText.getText());
+            if (lines < 0) {
+                throw new IllegalArgumentException("Lines is negative: " + lines);
+            }
+
+            fetcher.setLinesToCheckAdditionally(lines);
+            prefs.putInt("linesToCheckAdditionally", lines);
+
+            close();
+        });
+        cancelButton.addListener(SWT.Selection, e -> close());
+
+        shell.pack();
+    }
+}
